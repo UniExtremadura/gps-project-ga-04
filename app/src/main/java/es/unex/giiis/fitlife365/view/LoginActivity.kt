8dc368aae7f327407b1130a0021acacf1a95808c
Angel <aramirezf05@gmail.com>
2023-10-16 16:58:09 +0200

    LoginActivity

--- app/src/main/java/es/unex/giiis/fitlife365/view/LoginActivity.kt
+++ app/src/main/java/es/unex/giiis/fitlife365/view/LoginActivity.kt
@@ -1,5 +1,30 @@
 package es.unex.giiis.fitlife365.view
 
-class LoginActivity {
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import es.unex.giiis.fitlife365.R
 
+class LoginActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mainScreen)
+
+        val btnIniciarSesion = findViewById<Button>(R.id.iniciarSesionButton)
+        val btnRegistrarse = findViewById<Button>(R.id.registrateButton)
+
+        btnIniciarSesion.setOnClickListener {
+            // Navegar a la pantalla de inicio de sesión
+            val intent = Intent(this, IniciarSesionActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnRegistrarse.setOnClickListener {
+            // Navegar a la pantalla de registro
+            val intent = Intent(this, RegistroActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
