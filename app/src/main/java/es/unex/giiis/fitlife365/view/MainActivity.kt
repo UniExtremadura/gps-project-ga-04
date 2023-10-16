3c5d7a5608594ff8d16a0696c5ae5dd79fa1d458
Angel <aramirezf05@gmail.com>
2023-10-16 18:08:04 +0200

    Link LoginActivity y RegistroActivity

--- app/src/main/java/es/unex/giiis/fitlife365/view/MainActivity.kt
+++ app/src/main/java/es/unex/giiis/fitlife365/view/MainActivity.kt
@@ -1,12 +1,29 @@
 package es.unex.giiis.fitlife365.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import es.unex.giiis.fitlife365.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_screen)
+
+        val btnIniciarSesion = findViewById<Button>(R.id.iniciarSesionButton)
+        val btnRegistrarse = findViewById<Button>(R.id.registrateButton)
+
+        btnIniciarSesion.setOnClickListener {
+            // Navegar a la pantalla de inicio de sesión
+            val intent = Intent(this, IniciarSesionActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnRegistrarse.setOnClickListener {
+            // Navegar a la pantalla de registro
+            val intent = Intent(this, RegistroActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/es/unex/giiis/fitlife365/view/RegistroActivity.kt
@@ -0,0 +1,13 @@
+package es.unex.giiis.fitlife365.view
+
+import android.app.Activity
+import android.os.Bundle
+import es.unex.giiis.fitlife365.R
+
+class RegistroActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Cargar el diseño de activity_login.xml en es.unex.giiis.fitlife365.view.IniciarSesionActivity
+        setContentView(R.layout.activity_login)
+    }
+}
