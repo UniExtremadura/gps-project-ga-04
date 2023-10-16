a3c9572a4e6e70235a5afe1c5ef61179ec679fb5
Angel <aramirezf05@gmail.com>
2023-10-16 16:29:19 +0200

    Creación de carpetas

--- app/src/main/java/es/unex/giiis/fitlife365/view/MainActivity.kt
+++ app/src/main/java/es/unex/giiis/fitlife365/view/MainActivity.kt
@@ -1,15 +1,12 @@
-package es.unex.giiis.fitlife365
+package es.unex.giiis.fitlife365.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-//prueba
+import es.unex.giiis.fitlife365.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-
 }
