335320b68dc757f5254785f96a57ab218af27629
Angel Ramírez Flores <92265677+aramirezf05@users.noreply.github.com>
2023-10-16 16:20:38 +0200

    First Commit completo

---
+++ app/src/main/java/es/unex/giiis/fitlife365/MainActivity.kt
@@ -0,0 +1,15 @@
+package es.unex.giiis.fitlife365
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//prueba
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
