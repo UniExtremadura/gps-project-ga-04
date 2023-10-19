c9b252dd2a460e3ddeeeca5f3f8e1078dc6922bc
Angel <aramirezf05@gmail.com>
2023-10-19 11:54:54 +0200

    Edit Main Screen. Creación de register, rutinas, mis rutinas, ajustes. Iconos de perfil y papelera.

--- app/src/main/java/es/unex/giiis/fitlife365/view/RegistroActivity.kt
+++ app/src/main/java/es/unex/giiis/fitlife365/view/RegistroActivity.kt
@@ -7,7 +7,6 @@ import es.unex.giiis.fitlife365.R
 class RegistroActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Cargar el diseño de activity_login.xml en es.unex.giiis.fitlife365.view.IniciarSesionActivity
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.routine_screen)
     }
 }
