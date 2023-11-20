fad4b829e375f7b3342e5a40d3bcc9db1f0b524d
daniel <dmoyamen@alumnos.unex.es>
2023-11-20 16:12:55 +0100

    SCRUM-54 Desarrollar la funcionalidad de editar Perfil

--- app/src/main/java/es/unex/giiis/fitlife365/view/home/EditarPerfilFragment.kt
+++ app/src/main/java/es/unex/giiis/fitlife365/view/home/EditarPerfilFragment.kt
@@ -1,12 +1,28 @@
-package es.unex.giiis.fitlife365.view.home
+package es.unex.giiis.fitlife365
 
+import androidx.appcompat.app.AlertDialog
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import es.unex.giiis.fitlife365.R
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
+import es.unex.giiis.fitlife365.database.FitLife365Database
 import es.unex.giiis.fitlife365.model.User
+import es.unex.giiis.fitlife365.view.home.CrearRutinaFragment
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -14,9 +30,14 @@ import es.unex.giiis.fitlife365.model.User
  * create an instance of this fragment.
  */
 class EditarPerfilFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var editTextNombre: EditText
+    private lateinit var spinnerSexo: Spinner
+    private lateinit var editTextEdad: EditText
+    private lateinit var editTextAltura: EditText
+    private lateinit var editTextPeso: EditText
+    private lateinit var btnAceptar: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +51,66 @@ class EditarPerfilFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_editar_perfil, container, false)
+        val view = inflater.inflate(R.layout.fragment_editar_perfil, container, false)
+        val sexo = listOf("Hombre", "Mujer", "Otro")
+
+        // Obtener referencias a las vistas
+        editTextNombre = view.findViewById(R.id.et_nombre)
+        spinnerSexo = view.findViewById(R.id.spinnerMaterial)
+        editTextEdad = view.findViewById(R.id.et_edad)
+        editTextAltura = view.findViewById(R.id.et_estatura)
+        editTextPeso = view.findViewById(R.id.et_peso)
+        btnAceptar = view.findViewById(R.id.btnAceptar)
+
+        val adapterSexo = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, sexo)
+        adapterSexo.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerSexo.adapter = adapterSexo
+
+        // Obtener el usuario del Bundle
+        val user = arguments?.getSerializable("LOGIN_USER") as? User
+
+        if (user != null) {
+            editTextNombre.setText(user.name)
+            editTextEdad.setText(user.edad.toString())
+            val sexoIndex = sexo.indexOf(user.sexo)
+            spinnerSexo.setSelection(if (sexoIndex != -1) sexoIndex else 0)
+            editTextAltura.setText(user.altura.toString())
+            editTextPeso.setText(user.peso.toString())
+        }
+
+        // Configurar el evento de clic para el botón Aceptar
+        btnAceptar.setOnClickListener {
+            if (user != null) {
+                actualizarUsuario(user)
+            }
+        }
+
+        return view
+    }
+
+    private fun actualizarUsuario(user: User) {
+        // Obtener los valores de las vistas
+        val userId = user.userId
+        val nombre = editTextNombre.text.toString()
+        val sexo = spinnerSexo.selectedItem.toString()
+        val edad = editTextEdad.text.toString().toIntOrNull() ?: 0
+        val altura = editTextAltura.text.toString().toIntOrNull() ?: 0
+        val peso = editTextPeso.text.toString().toIntOrNull() ?: 0
+
+        // Llamar al método updateUser del UserDao
+        GlobalScope.launch(Dispatchers.IO) {
+            // Asegúrate de obtener la instancia de la base de datos correctamente
+            val userDao = FitLife365Database.getInstance(requireContext())?.userDao()
+            if (userDao != null) {
+                if (userId != null) {
+                    userDao.updateUser(userId, nombre, sexo, edad, altura, peso)
+                    // Mostrar un mensaje Toast después de la actualización
+                    GlobalScope.launch(Dispatchers.Main) {
+                        Toast.makeText(requireContext(), "Perfil actualizado", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
     }
 
     companion object {
