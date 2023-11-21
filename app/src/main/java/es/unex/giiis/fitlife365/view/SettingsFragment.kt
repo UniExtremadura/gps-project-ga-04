d6bee259c4868ef3ef85ba05ff53bb2a71ec9950
daniel <dmoyamen@alumnos.unex.es>
2023-11-21 13:13:45 +0100

    SCRUM-60-Desarrollar la funcionalidad para seleccionar entre temas visuales predefinidos

--- app/src/main/java/es/unex/giiis/fitlife365/view/SettingsFragment.kt
+++ app/src/main/java/es/unex/giiis/fitlife365/view/SettingsFragment.kt
@@ -1,8 +1,13 @@
 package es.unex.giiis.fitlife365.view
 
+import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
+import androidx.preference.SwitchPreference
 import es.unex.giiis.fitlife365.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
@@ -10,6 +15,19 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
 
+        // Obtener la preferencia del tema
+        val themeSwitchPreference = findPreference<SwitchPreference>("theme")
+        themeSwitchPreference?.setOnPreferenceChangeListener { _, newValue ->
+            val sharedPreferences = context?.let { PreferenceManager.getDefaultSharedPreferences(it) }
+            if (sharedPreferences != null) {
+                sharedPreferences.edit().putBoolean("theme_preference", newValue as Boolean).apply()
+            }
+
+            // Mostrar un Toast indicando que la aplicación necesita reiniciarse
+            Toast.makeText(context, "Reinicie la aplicación para aplicar los cambios de tema", Toast.LENGTH_SHORT).show()
+
+            true
+        }
 
     }
 }
