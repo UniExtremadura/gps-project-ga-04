18b651515f4b8f2a53c195d3a919c845cda51cf0
Angel <aramirezf05@gmail.com>
2023-11-25 11:26:10 +0100

    SCRUM-28 Implementar el flujo de registro

--- app/src/main/java/es/unex/giiis/fitlife365/view/LoginRegister/RegistroActivity.kt
+++ app/src/main/java/es/unex/giiis/fitlife365/view/LoginRegister/RegistroActivity.kt
@@ -1,6 +1,5 @@
 package es.unex.giiis.fitlife365.view.LoginRegister
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -12,13 +11,12 @@ import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
+import androidx.appcompat.app.AppCompatActivity
 import es.unex.giiis.fitlife365.R
-import es.unex.giiis.fitlife365.databinding.ActivityLoginBinding
 import es.unex.giiis.fitlife365.databinding.ActivityRegisterBinding
 import es.unex.giiis.fitlife365.model.User
-import es.unex.giiis.fitlife365.view.home.MisRutinasActivity
 
-class RegistroActivity : Activity() {
+class RegistroActivity : AppCompatActivity() {
 
     private lateinit var btnContinuar: Button
     private lateinit var registerUsername: EditText
@@ -27,6 +25,9 @@ class RegistroActivity : Activity() {
     private lateinit var registerConfirmPassword: EditText
     private lateinit var checkBoxPassword: CheckBox
     private lateinit var binding: ActivityRegisterBinding
+
+
+
 
     companion object {
         const val USERNAME = "USERNAME"
@@ -51,8 +52,11 @@ class RegistroActivity : Activity() {
         registerConfirmPassword = findViewById(R.id.registerConfirmPassword)
         checkBoxPassword = findViewById(R.id.checkBoxPassword)
 
+
+
         setUpListeners()
     }
+
 
     private fun validateFields() : Boolean {
         val username = registerUsername.text.toString()
@@ -104,22 +108,28 @@ class RegistroActivity : Activity() {
             }
 
             btnContinuar.setOnClickListener {
-                // Realizar la validación de los campos antes de continuar
-                if (!validateFields()) {
-                    showToast("Por favor, completa todos los campos correctamente.")
-                    return@setOnClickListener
-                }
-                navigateToLogin(User(registerUsername.text.toString(), registerPassword.text.toString()))
+                join()
             }
         }
     }
 
+    private fun join() {
+        with(binding) {
+            val check = validateFields()
+            if (check) {
+                val user = User(registerUsername.text.toString(), registerPassword.text.toString())
+                navigateToLogin(user)
+            }
+        }
+    }
+
+
     private fun navigateToLogin(user: User) {
         val intent = Intent(this, IniciarSesionActivity::class.java).apply {
-            putExtra(USERNAME, user.name)
-            putExtra(PASSWORD, user.password)
+            putExtra("LOGIN_USER", user)
         }
         startActivity(intent)
     }
+
 
 }
