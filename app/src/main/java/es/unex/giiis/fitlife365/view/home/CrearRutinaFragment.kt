754bfadb01321a833b152d61644d3a96abddc68d
Angel <aramirezf05@gmail.com>
2023-11-25 12:03:28 +0100

    SCRUM-35 Implementar la función de crear rutina

--- app/src/main/java/es/unex/giiis/fitlife365/view/home/CrearRutinaFragment.kt
+++ app/src/main/java/es/unex/giiis/fitlife365/view/home/CrearRutinaFragment.kt
@@ -1,16 +1,29 @@
 package es.unex.giiis.fitlife365.view.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.preference.PreferenceManager
 import es.unex.giiis.fitlife365.R
+import es.unex.giiis.fitlife365.database.FitLife365Database
+import es.unex.giiis.fitlife365.model.Routine
+import es.unex.giiis.fitlife365.model.User
+import kotlinx.coroutines.launch
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+const val ARG_PARAM1 = "param1"
+const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -21,6 +34,11 @@ class CrearRutinaFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit  var btnAceptar: View
+    private lateinit var nombreRutina : EditText
+    private lateinit var pesoObjetivoRutina : EditText
+    private lateinit var currentUser : User
+    private lateinit var listaEjerciciosFragment: ListaEjerciciosFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,8 +52,77 @@ class CrearRutinaFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_crear_rutina, container, false)
+        //Inicializar la base de datos
+        val database = FitLife365Database.getInstance(requireContext())
+        val routineDao = database?.routineDao()
+        val view = inflater.inflate(R.layout.fragment_crear_rutina, container, false)
+
+        nombreRutina = view.findViewById(R.id.editTextText)
+        pesoObjetivoRutina = view.findViewById(R.id.editTextNumber)
+
+
+        val diasEntrenamiento = listOf("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo")
+        val experiencia = listOf("Principiante", "Intermedio", "Avanzado")
+
+        // Configurar el Spinner para los días de entrenamiento
+        val spinnerDiasEntrenamiento: Spinner = view.findViewById(R.id.spinnerDiasEntrenamiento)
+        val adapterDiasEntrenamiento = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, diasEntrenamiento)
+        adapterDiasEntrenamiento.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerDiasEntrenamiento.adapter = adapterDiasEntrenamiento
+
+        // Configurar el Spinner para la experiencia
+        val spinnerExperiencia: Spinner = view.findViewById(R.id.spinnerExperiencia)
+        val adapterExperiencia = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, experiencia)
+        adapterExperiencia.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerExperiencia.adapter = adapterExperiencia
+
+        // Inicializa el botón
+        btnAceptar = view.findViewById(R.id.btnAceptar)
+
+        // Establece el OnClickListener
+        btnAceptar.setOnClickListener {
+
+            // Crea la rutina
+            val nombre = "Por defecto"
+            val nombreRutina = nombreRutina.text.toString().takeIf { it.isNotEmpty() } ?: nombre
+
+            val peso = 60
+            val pesoObjetivoRutina = pesoObjetivoRutina.text.toString().toIntOrNull() ?: peso
+
+            val diasEntrenamiento = spinnerDiasEntrenamiento.selectedItem.toString()
+
+            val routine = Routine(
+                routineId = null,
+                userId = currentUser.userId,
+                name = nombreRutina,
+                pesoObjetivo = pesoObjetivoRutina,
+                diasEntrenamiento = diasEntrenamiento,
+                ejercicios = ""
+            )
+
+
+            // Inserta la rutina en la base de datos
+            if (routineDao != null) {
+                lifecycleScope.launch {
+                    val id = routineDao.insert(routine)
+                    routine.routineId = id
+                }
+            }
+
+            val difficulty = spinnerExperiencia.selectedItem.toString()
+
+            activity?.supportFragmentManager?.beginTransaction()?.apply {
+                replace(R.id.fragment_containerHome, listaEjerciciosFragment)
+                addToBackStack(null) // Agrega la transacción a la pila de retroceso
+                commit()
+            }
+        }
+
+        return view
+    }
+
+    fun setUser (user: User){
+        currentUser = user
     }
 
     companion object {
