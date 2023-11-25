42051e8071226cc9820a6db969fea7bbb539a18e
samue <satrinida@alumnos.unex.es>
2023-11-25 16:51:28 +0100

    SCRUM-82 Implementar la función de consejos de seguridad

--- app/src/main/java/es/unex/giiis/fitlife365/view/home/PersonalTrainerFragment.kt
+++ app/src/main/java/es/unex/giiis/fitlife365/view/home/PersonalTrainerFragment.kt
@@ -32,6 +32,7 @@ class PersonalTrainerFragment : Fragment() {
     private lateinit var textViewTelefono: TextView
     private lateinit var imagenesEntrenadoresArray: TypedArray
     private var isDatePickerVisible = false
+    private lateinit var buttonConsejos: Button
 
 
     private var selectedYear: Int = 0
@@ -61,11 +62,18 @@ class PersonalTrainerFragment : Fragment() {
         textViewNombreSeleccionado = view.findViewById(R.id.textViewNombreSeleccionado)
         textViewTelefono = view.findViewById(R.id.textViewTelefono)
         textView = view.findViewById(R.id.textView)
+        buttonConsejos = view.findViewById(R.id.buttonConsejos)
 
 
         entrenadoresArray = resources.getStringArray(R.array.entrenadores_personales)
         telefonosArray = resources.getStringArray(R.array.telefonos_entrenadores)
         imagenesEntrenadoresArray = resources.obtainTypedArray(R.array.imagenes_entrenadores)
+
+        buttonConsejos.setOnClickListener {
+            // Abre la actividad de Consejos aquí
+            val intent = Intent(requireContext(), ConsejosActivity::class.java)
+            startActivity(intent)
+        }
 
 
         // Configura el evento para mostrar la lista de entrenadores
@@ -205,6 +213,7 @@ class PersonalTrainerFragment : Fragment() {
         // Muestra el cuadro de diálogo
         builder.show()
     }
+
     // Libera este recurso
     override fun onDestroyView() {
         super.onDestroyView()
