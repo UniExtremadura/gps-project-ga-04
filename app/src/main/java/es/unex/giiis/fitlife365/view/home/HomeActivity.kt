17b34f0d1fa5a22446e7176868d030e8191a66a3
Angel <aramirezf05@gmail.com>
2023-11-25 13:06:49 +0100

    Aplicando cambios con el Merge anterior y actualizando el AndroidManifest, HomeActivity e iconos

--- app/src/main/java/es/unex/giiis/fitlife365/view/home/HomeActivity.kt
+++ app/src/main/java/es/unex/giiis/fitlife365/view/home/HomeActivity.kt
@@ -4,10 +4,20 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
 import es.unex.giiis.fitlife365.R
+import es.unex.giiis.fitlife365.databinding.ActivityHomeBinding
 import es.unex.giiis.fitlife365.model.User
+import es.unex.giiis.fitlife365.view.SettingsActivity
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var crearRutinaFragment: CrearRutinaFragment
+    private lateinit var misRutinasFragment: MisRutinasFragment
 
     companion object {
         const val LOGIN_USER = "LOGIN_USER"
@@ -20,6 +30,43 @@ class HomeActivity : AppCompatActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpUI()
+        setUpListeners()
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_containerHome, fragment)
+            commit()
+    }
+
+    private fun setUpUI() {
+        crearRutinaFragment = CrearRutinaFragment()
+        crearRutinaFragment.setUser(intent.getSerializableExtra(LOGIN_USER) as User)
+        misRutinasFragment = MisRutinasFragment.newInstance(intent.getSerializableExtra(LOGIN_USER) as User)
+
+        setCurrentFragment(misRutinasFragment)
+    }
+    private fun setUpListeners() {
+        with(binding){
+            usernameText.setOnClickListener {
+                navigateToSettings()
+            }
+            bottomNavigation.setOnItemSelectedListener{
+                when(it.itemId){
+                    es.unex.giiis.fitlife365.R.id.nav_create_routine -> setCurrentFragment(crearRutinaFragment)
+                    es.unex.giiis.fitlife365.R.id.nav_myroutines -> setCurrentFragment(misRutinasFragment)
+                }
+                true
+            }
+        }
+    }
+
+    private fun navigateToSettings() {
+        val intent = Intent(this, SettingsActivity::class.java)
+        startActivity(intent)
     }
 }
