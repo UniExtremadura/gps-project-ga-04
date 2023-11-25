415f4b0e2bb552661c73811dbf1092094ee632c4
samue <satrinida@alumnos.unex.es>
2023-11-25 13:03:17 +0100

    SCRUM-47 Diseñar la función de actualizar la evaluación de salud

--- app/src/main/java/es/unex/giiis/fitlife365/view/home/HomeActivity.kt
+++ app/src/main/java/es/unex/giiis/fitlife365/view/home/HomeActivity.kt
@@ -4,10 +4,20 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import es.unex.giiis.fitlife365.R
+import es.unex.giiis.fitlife365.databinding.ActivityHomeBinding
 import es.unex.giiis.fitlife365.model.User
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
+    private lateinit var imageViewEvS: ImageView
+    private lateinit var usernameTextEvS: TextView
+    private lateinit var binding: ActivityHomeBinding
+
 
     companion object {
         const val LOGIN_USER = "LOGIN_USER"
@@ -20,6 +30,35 @@ class HomeActivity : AppCompatActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_home)
+
+        toolbar = findViewById(R.id.toolbar3)
+        imageViewEvS = findViewById(R.id.imageViewEvS)
+        usernameTextEvS = findViewById(R.id.usernameTextEvS)
+
+        setUpListeners()
+
+    }
+
+
+    private fun setUpListeners() {
+        with(binding){
+
+            imageViewEvS.setOnClickListener {
+                navigateToEvaluacionSalud()
+            }
+            usernameTextEvS.setOnClickListener {
+                navigateToEvaluacionSalud()
+            }
+        }
+    }
+    private fun navigateToEvaluacionSalud() {
+        val user = intent.getSerializableExtra(LOGIN_USER) as? User
+        val intent = Intent(this, EvaluacionSaludActivity::class.java).apply {
+            // Pasa el usuario como parte de los datos del intent
+            putExtra("LOGIN_USER", user)
+        }
+        startActivity(intent)
     }
 }
