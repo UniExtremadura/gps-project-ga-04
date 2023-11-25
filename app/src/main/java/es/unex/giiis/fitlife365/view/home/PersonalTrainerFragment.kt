1937daf377ba731b9729d1306326f689d154c551
samue <satrinida@alumnos.unex.es>
2023-11-25 13:50:02 +0100

    SCRUM-77 Implementar la funcionalidad para programar una consulta virtual, incluyendo la selección de hora

--- app/src/main/java/es/unex/giiis/fitlife365/view/home/PersonalTrainerFragment.kt
+++ app/src/main/java/es/unex/giiis/fitlife365/view/home/PersonalTrainerFragment.kt
@@ -19,9 +19,10 @@ import java.util.Locale
 
 class PersonalTrainerFragment : Fragment() {
 
-    private lateinit var datePicker: DatePicker
     private lateinit var timePicker: TimePicker
+    private lateinit var datePicker: DatePicker
     private lateinit var buttonDia: Button
+    private lateinit var buttonHora: Button
     private lateinit var buttonGuardarDia: Button
     private lateinit var imageView: ImageView
     private lateinit var entrenadoresArray: Array<String>
@@ -36,6 +37,8 @@ class PersonalTrainerFragment : Fragment() {
     private var selectedYear: Int = 0
     private var selectedMonth: Int = 0
     private var selectedDay: Int = 0
+    private var selectedHour: Int = 0
+    private var selectedMinute: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,8 +52,10 @@ class PersonalTrainerFragment : Fragment() {
             Toast.LENGTH_LONG
         ).show()
 
+        timePicker = view.findViewById(R.id.timePicker)
         datePicker = view.findViewById(R.id.datePicker)
         buttonDia = view.findViewById(R.id.button6)
+        buttonHora = view.findViewById(R.id.button7)
         buttonGuardarDia = view.findViewById(R.id.buttonGuardarDia)
         imageView = view.findViewById(R.id.imageView8)
         textViewNombreSeleccionado = view.findViewById(R.id.textViewNombreSeleccionado)
@@ -62,11 +67,25 @@ class PersonalTrainerFragment : Fragment() {
         telefonosArray = resources.getStringArray(R.array.telefonos_entrenadores)
         imagenesEntrenadoresArray = resources.obtainTypedArray(R.array.imagenes_entrenadores)
 
+
         // Configura el evento para mostrar la lista de entrenadores
         imageView.setOnClickListener {
             showEntrenadoresList()
         }
 
+        // Configura el evento de selección de hora
+        timePicker.setOnTimeChangedListener { _, hourOfDay, minute ->
+            // Almacena la hora y los minutos seleccionados
+            selectedHour = hourOfDay
+            selectedMinute = minute
+
+            // Oculta el TimePicker y muestra los elementos necesarios
+            timePicker.visibility = View.GONE
+            showTrainerDetails()
+
+            // Desplaza el foco de la vista hacia otro elemento, por ejemplo, el botón de día
+            buttonDia.requestFocus()
+        }
 
         // Configura el evento de selección de fecha
         datePicker.setOnDateChangedListener { _, year, monthOfYear, dayOfMonth ->
@@ -92,6 +111,19 @@ class PersonalTrainerFragment : Fragment() {
             }
         }
 
+        // Configura el evento para mostrar el TimePicker
+        buttonHora.setOnClickListener {
+            hideTrainerDetails()
+            timePicker.visibility = View.VISIBLE
+            datePicker.visibility = View.GONE
+
+            val yOffset = 50 // Cambia este valor según tus necesidades
+            // Mueve el TimePicker hacia abajo para que sea más visible
+            timePicker.translationY = yOffset.toFloat()
+
+            // Oculta el botón para guardar día
+            // buttonGuardarDia.visibility = View.GONE
+        }
 
         // Configura el evento para mostrar el DatePicker
         buttonDia.setOnClickListener {
@@ -132,7 +164,14 @@ class PersonalTrainerFragment : Fragment() {
         textView.visibility = View.VISIBLE // Muestra el mensaje "ENTRENADOR PERSONAL"
 
         // Muestra la hora seleccionada en un TextView o realiza otra acción según tus necesidades
-
+        val formattedTime =
+            String.format(Locale.getDefault(), "Hora Seleccionada: %02d:%02d", selectedHour, selectedMinute)
+        Toast.makeText(
+            requireContext(),
+            "Para modificar la hora, seleccione el numero a modificar. Seleccione AM/PM",
+            Toast.LENGTH_LONG
+        ).show()
+        textView.text = formattedTime
         textView.visibility = View.VISIBLE
     }
 
