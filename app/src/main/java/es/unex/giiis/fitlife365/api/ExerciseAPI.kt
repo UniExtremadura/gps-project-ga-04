4ae893919ca14e50285e61f1372739a15a0ab63a
Muelas <cmartintv@alumnos.unex.es>
2023-11-26 11:45:16 +0100

    SCRUM-86: Importar y administrar la API con los diferentes ejercicios

---
+++ app/src/main/java/es/unex/giiis/fitlife365/api/ExerciseAPI.kt
@@ -0,0 +1,50 @@
+package es.unex.giiis.fitlife365.api
+
+import android.telecom.Call
+import es.unex.giiis.fitlife365.data.Exercise
+import es.unex.giiis.fitlife365.data.ExerciseList
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+//https://api.api-ninjas.com/v1/exercises?X-Api-Key=wfr49mRVMN8mvCFkbYj5Jw==kfHUsZc2rKbpMCJT&difficulty=beginner
+private const val API_KEY = "X-Api-Key=wfr49mRVMN8mvCFkbYj5Jw==kfHUsZc2rKbpMCJT"
+
+private val service: ExerciseAPI by lazy {
+    val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor())
+        .build()
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://api.api-ninjas.com/v1/")
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    retrofit.create(ExerciseAPI::class.java)
+}
+
+fun getNetworkService() = service
+
+class APIError(message: String, cause: Throwable?) : Throwable(message, cause)
+
+interface ExerciseAPI {
+    @GET("exercises?" + API_KEY + "&")
+    suspend fun getExercisesByMuscle(@Query("muscle") muscle: String): ExerciseList
+
+    @GET("exercises?" + API_KEY + "&")
+    suspend fun getExercisesByEquipment(@Query("equipment") equipment: String): ExerciseList
+
+    @GET("exercises?" + API_KEY + "&")
+    suspend fun getExercisesByDifficulty(@Query("difficulty") difficulty: String): ExerciseList
+
+    @GET("exercises?" + API_KEY + "&")
+    suspend fun getExercisesByType(@Query("type") type: String): ExerciseList
+
+    @GET("exercises?" + API_KEY + "&")
+    suspend fun getExercisesByMuscleAndDifficulty(@Query("muscle") muscle: String, @Query("difficulty") difficulty: String): ExerciseList
+
+}
---
+++ app/src/main/java/es/unex/giiis/fitlife365/data/Mappers.kt
@@ -0,0 +1,18 @@
+package es.unex.giiis.fitlife365.data
+
+import es.unex.giiis.fitlife365.model.ExerciseModel
+
+fun ExerciseList.toExercise(): List<ExerciseModel> {
+    return mapNotNull { exercise ->
+        ExerciseModel(
+            exerciseId = null,
+            name = exercise.name.orEmpty(),
+            type = exercise.type.orEmpty(),
+            muscle = exercise.muscle.orEmpty(),
+            equipment = exercise.equipment.orEmpty(),
+            difficulty = exercise.difficulty.orEmpty(),
+            instructions = exercise.instructions.orEmpty(),
+            isSelected = false
+        )
+    }
+}
