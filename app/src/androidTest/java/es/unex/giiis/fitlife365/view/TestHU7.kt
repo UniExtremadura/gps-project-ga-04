88d06554689ff02750cde3ad1096a084cb38e979
Angel <aramirezf05@gmail.com>
2023-12-07 18:31:43 +0100

    Test HU7 realizado

---
+++ app/src/androidTest/java/es/unex/giiis/fitlife365/view/TestHU7.kt
@@ -0,0 +1,254 @@
+package es.unex.giiis.fitlife365.view
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import es.unex.giiis.fitlife365.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class TestHU7 {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun testHU7() {
+        val materialButton = onView(
+            allOf(
+                withId(R.id.registrateButton), withText("Regístrate"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton.perform(click())
+
+        val appCompatEditText = onView(
+            allOf(
+                withId(R.id.registerUsername),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText.perform(replaceText("prueba"), closeSoftKeyboard())
+
+        val appCompatEditText2 = onView(
+            allOf(
+                withId(R.id.registerEmail),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText2.perform(replaceText("prueba@gmail.com"), closeSoftKeyboard())
+
+        val appCompatEditText3 = onView(
+            allOf(
+                withId(R.id.registerPassword),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText3.perform(replaceText("prueba"), closeSoftKeyboard())
+
+        val appCompatEditText4 = onView(
+            allOf(
+                withId(R.id.registerConfirmPassword),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    4
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText4.perform(replaceText("prueba"), closeSoftKeyboard())
+
+        val materialButton2 = onView(
+            allOf(
+                withId(R.id.idContinuarRegistro), withText("Continuar"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton2.perform(click())
+
+        val materialButton3 = onView(
+            allOf(
+                withId(R.id.btnAceptar), withText("Aceptar"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    6
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton3.perform(click())
+
+        val appCompatImageView = onView(
+            allOf(
+                withId(R.id.imageViewResultado),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatImageView.perform(click())
+
+        val bottomNavigationItemView = onView(
+            allOf(
+                withId(R.id.nav_create_routine), withContentDescription("Crear Rutina"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.bottom_navigation),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView.perform(click())
+
+        val textView = onView(
+            allOf(
+                withId(R.id.textView2), withText("NUEVA RUTINA"),
+                withParent(withParent(withId(R.id.fragment_containerHome))),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("NUEVA RUTINA")))
+
+        val appCompatEditText5 = onView(
+            allOf(
+                withId(R.id.editTextText),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.fragment_containerHome),
+                        0
+                    ),
+                    11
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText5.perform(replaceText("rutina 1"), closeSoftKeyboard())
+
+        val appCompatEditText6 = onView(
+            allOf(
+                withId(R.id.editTextNumber),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.fragment_containerHome),
+                        0
+                    ),
+                    12
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatEditText6.perform(replaceText("70"), closeSoftKeyboard())
+
+        val materialButton4 = onView(
+            allOf(
+                withId(R.id.btnAceptar), withText("Aceptar"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.fragment_containerHome),
+                        0
+                    ),
+                    10
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton4.perform(click())
+
+        val textView2 = onView(
+            allOf(
+                withId(android.R.id.text1), withText("Selecciona el músculo"),
+                withParent(
+                    allOf(
+                        withId(R.id.spinnerMusculo),
+                        withParent(IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        textView2.check(matches(withText("Selecciona el músculo")))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
