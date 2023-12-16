7f64b82d584e4016c7a3219f1f03d01d66dd604d
Angel <aramirezf05@gmail.com>
2023-12-16 14:12:29 +0100

    ViewModel de MisRutinasFragment

--- app/src/main/java/es/unex/giiis/fitlife365/view/home/MisRutinasViewModel.kt
+++ app/src/main/java/es/unex/giiis/fitlife365/view/home/MisRutinasViewModel.kt
@@ -1,6 +1,7 @@
 package es.unex.giiis.fitlife365.view.home
 
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -14,13 +15,31 @@ import es.unex.giiis.fitlife365.model.User
 import kotlinx.coroutines.launch
 
 class MisRutinasViewModel (private val repository: Repository) : ViewModel() {
-    suspend fun getRoutinesByUser(userId: Long?): List<Routine>? {
-        return repository.routineDao.getRoutinesByUser(userId)
+
+    private val _rutinasList = MutableLiveData<List<Routine>>()
+    val rutinasList: LiveData<List<Routine>>
+        get() = _rutinasList
+
+    private val _textEmptyVisibility = MutableLiveData<Int>()
+    val textEmptyVisibility: LiveData<Int>
+        get() = _textEmptyVisibility
+
+    fun getRoutinesByUser(userId: Long?) {
+        viewModelScope.launch {
+            try {
+                val routines = repository.getRoutinesByUser(userId) ?: emptyList()
+                _rutinasList.value = routines
+                _textEmptyVisibility.value = if (routines.isEmpty()) View.VISIBLE else View.GONE
+            } catch (error: Throwable) {
+                _toast.value = error.message
+            }
+        }
     }
 
     private val _toast = MutableLiveData<String?>()
     val toast: LiveData<String?>
         get() = _toast
+
 
 
     companion object { val Factory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
